6f1791b6fe9e2e1c515d313e3d39e83f981b4492
Luiz <luizhmartins11@gmail.com>
2023-05-16 14:44:39 -0300

    criação da classe CalculadoraLimiteCredito

---
+++ src/cap4InstrucoesDeControleParteI/exercicios/domain/CalculadoraLimiteCredito.java
@@ -0,0 +1,21 @@
+package cap4InstrucoesDeControleParteI.exercicios.domain;
+
+public class CalculadoraLimiteCredito {
+    public int numeroConta;
+    public int saldoMes;
+    public int totalItensCobrados;
+    public int totalCreditos;
+    public int limiteCreditoAutorizado;
+
+    public int saldoMensal(){
+        int saldoFinal = saldoMes + totalItensCobrados - totalCreditos;
+        return saldoFinal;
+    }
+    public boolean limiteExcedido(){
+        if (saldoMensal() <= this.limiteCreditoAutorizado){
+            System.out.println("Limite de credito nao excedido");
+            return false;
+        } else
+            return true;
+    }
+}
